58a6f740b68310667d61673db52fdf2239d772cf
quvarxa <michaelchesser@outlook.com>
2014-11-01 22:59:43 +1030

    Rust version update

--- src/lib.rs
+++ src/lib.rs
@@ -1,43 +1,25 @@
-#![crate_name = "bitmap"]
-#![crate_type = "lib"]
-
 use std::io;
 
 /// Main bitmap structure
 pub struct Bitmap {
-    // The width of the bitmap in pixels, as an unsigned integer (4 bytes).
     width:  i32,
-
-    // The height of the bitmap in pixels, as an unsigned integer (4 bytes).
     height: i32,
-
-    // The pixel array, each row of the array is padded to 4 bytes in size.
     pixels: Vec<u8>,
 }
 
 impl Bitmap {
     /// Create a new blank (white) bitmap of a specified size.
-    ///
-    /// # Arguments
-    /// `width` - The width of the bitmap.
-    /// `height` - The height of the bitmap.
     pub fn new(width: i32, height: i32) -> Bitmap {
         Bitmap {
             width:  width,
             height: height,
-
             // Create a vector to store the pixels in, ensuring that it is padded to a multiple of 4
             // bytes of each row.
             pixels: Vec::from_elem((height * (width * 3 + width % 4)) as uint, 0xFFu8)
         }
     }
 
-    /// Set a pixel at (x, y) to a specified color.
-    ///
-    /// # Arguments
-    /// `x` - The x coordinate of the pixel.
-    /// `y` - The y coordinate of the pixel.
-    /// `color` - color values in the form (r, g, b).
+    /// Set a pixel at (x, y) to a specified color (r, g, b).
     pub fn set_pixel(&mut self, x: i32, y: i32, color: (u8, u8, u8)) {
         // Calculate the byte offset for x
         let i = ((self.height - y - 1) * (self.width * 3) + x * 3) as uint;
@@ -50,19 +32,16 @@ impl Bitmap {
     }
 
     /// Write the stored data to a file with given filename.
-    ///
-    /// # Arguments
-    /// `filename` - The filename of the target file.
     pub fn write_to_file(&self, filename: &str) -> io::IoResult<()> {
-        static FILE_HEADER_SIZE:  u32 = 14;
-        static BMP_INFO_SIZE:     u32 = 40;
-        static TOTAL_HEADER_SIZE: u32 = FILE_HEADER_SIZE + BMP_INFO_SIZE;
+        const FILE_HEADER_SIZE: u32 = 14;
+        const BMP_INFO_SIZE: u32 = 40;
+        const TOTAL_HEADER_SIZE: u32 = FILE_HEADER_SIZE + BMP_INFO_SIZE;
 
         let image_size = (self.height * self.width*3 + self.height * (self.width % 4)) as u32;
         let file_size = image_size + TOTAL_HEADER_SIZE;
 
         // Bitmap file header
-        let file_header: [u8, ..FILE_HEADER_SIZE] = [
+        let file_header: [u8, ..FILE_HEADER_SIZE as uint] = [
             'B' as u8, 'M' as u8,
             file_size as u8, (file_size>>8) as u8, (file_size>>16) as u8, (file_size>>24) as u8,
             0, 0,
@@ -70,7 +49,7 @@ impl Bitmap {
             TOTAL_HEADER_SIZE as u8, 0, 0, 0
         ];
         // Bitmap information header
-        let info_header: [u8, ..BMP_INFO_SIZE] = [
+        let info_header: [u8, ..BMP_INFO_SIZE as uint] = [
             BMP_INFO_SIZE as u8, 0, 0, 0,
             self.width as u8, (self.width>>8) as u8, (self.width>>16) as u8, (self.width>>24) as u8,
             self.height as u8, (self.height>>8) as u8, (self.height>>16) as u8, (self.height>>24) as u8,
